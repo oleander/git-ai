dd3e4a0bdbddfd8e485cbeb6cca882b70a5d5373
Linus Oleander <linus.oleander@clark.de>
2023-10-07 14:21:00 +0200

    Add ansi_term dependency and refactor main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,65 +1,73 @@
 use std::process::{Command, exit};
+use ansi_term::Colour::{Green, Yellow};
 
 fn main() {
-    let color_green = "\x1b[32m";
-    let color_yellow = "\x1b[33m";
-    let color_reset = "\x1b[0m";
+    let files_to_add = get_git_status();
 
-    let git_status_output = Command::new("git")
-        .arg("status")
-        .arg("--porcelain")
-        .output()
-        .expect("Failed to execute git status")
-        .stdout;
-
-    let files_to_add = String::from_utf8_lossy(&git_status_output).to_string();
-
-    if files_to_add.trim().is_empty() {
+    if files_to_add.is_empty() {
         println!("No changes detected.");
         exit(0);
     }
 
-    let git_add_output = Command::new("git")
+    if !run_git_add() {
+        eprintln!("Error adding changes to git.");
+        exit(1);
+    }
+
+    if !run_git_commit() {
+        eprintln!("Error committing changes.");
+        exit(1);
+    }
+
+    let commit_message = get_latest_commit_message();
+
+    println!("{}:", Green.paint(commit_message));
+
+    for line in files_to_add {
+        println!("  {}", Yellow.paint(line));
+    }
+}
+
+fn get_git_status() -> Vec<String> {
+    let output = Command::new("git")
+        .arg("status")
+        .arg("--porcelain")
+        .output()
+        .expect("Failed to execute git status");
+
+    String::from_utf8_lossy(&output.stdout)
+        .lines()
+        .map(String::from)
+        .collect()
+}
+
+fn run_git_add() -> bool {
+    let output = Command::new("git")
         .arg("add")
         .arg(".")
         .output()
         .expect("Failed to execute git add");
 
-    if !git_add_output.status.success() {
-        eprintln!(
-            "Error adding changes to git: {}",
-            String::from_utf8_lossy(&git_add_output.stderr)
-        );
-        exit(1);
-    }
+    output.status.success()
+}
 
-    let git_commit_output = Command::new("git")
+fn run_git_commit() -> bool {
+    let output = Command::new("git")
         .arg("commit")
         .arg("--no-edit")
         .output()
         .expect("Failed to execute git commit");
 
-    if !git_commit_output.status.success() {
-        eprintln!(
-            "Error committing changes: {}",
-            String::from_utf8_lossy(&git_commit_output.stderr)
-        );
-        exit(1);
-    }
+    output.status.success()
+}
 
-    let commit_message = Command::new("git")
+fn get_latest_commit_message() -> String {
+    let output = Command::new("git")
         .arg("log")
         .arg("-1")
         .arg("--pretty=%B")
         .output()
-        .expect("Failed to execute git log")
-        .stdout;
+        .expect("Failed to execute git log");
 
-    let commit_message = String::from_utf8_lossy(&commit_message).trim().to_string();
-
-    println!("{}{}{}:", color_green, commit_message, color_reset);
-
-    for line in files_to_add.lines() {
-        println!("  {}{}{}", color_yellow, line, color_reset);
-    }
+    String::from_utf8_lossy(&output.stdout).trim().to_string()
 }
