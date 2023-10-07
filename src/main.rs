05368fbf6b11fcec5961d95832fb75b50e442d11
Linus Oleander <linus.oleander@clark.de>
2023-10-07 14:41:42 +0200

    Add git2 dependency and refine error handling

--- src/main.rs
+++ src/main.rs
@@ -1,82 +1,96 @@
-use std::process::{Command, exit};
+use git2::Repository;
+use std::env;
 use std::path::Path;
+use std::process::{exit, Command};
 
 fn main() {
-    if !ensure_aicommit_hooks_installed() {
-        eprintln!("Error: aicommit hooks are not installed.");
-        exit(1);
-    }
+  if !ensure_aicommit_hooks_installed() {
+    eprintln!("Error: aicommit hooks are not installed.");
+    exit(1);
+  }
 
-    let files_to_add = get_git_status();
+  let files_to_add = get_git_status();
 
-    if files_to_add.is_empty() {
-        println!("No changes detected.");
-        exit(0);
-    }
+  if files_to_add.is_empty() {
+    println!("No changes detected.");
+    exit(0);
+  }
 
-    if !run_git_add() {
-        eprintln!("Error adding changes to git.");
-        exit(1);
-    }
+  if should_add_all() && !run_git_add() {
+    eprintln!("Error adding changes to git.");
+    exit(1);
+  }
 
-    if !run_git_commit() {
-        eprintln!("Error committing changes.");
-        exit(1);
-    }
+  match run_git_commit() {
+    Ok(_) => {},
+    Err(err) => {
+      eprintln!("Error committing changes: {}", err);
+      exit(1);
+    },
+  }
 
-    let commit_message = get_latest_commit_message();
+  let commit_message = match get_latest_commit_message() {
+    Ok(message) => message,
+    Err(err) => {
+      eprintln!("Failed to fetch the latest commit message: {}", err);
+      exit(1);
+    },
+  };
 
-    println!("▶ {}", commit_message);
+  println!("▶ {}", commit_message);
 
-    for line in files_to_add {
-        println!("   🔸{}", line);
-    }
+  for line in files_to_add {
+    println!("   🔸{}", line);
+  }
 }
 
 fn get_git_status() -> Vec<String> {
-    let output = Command::new("git")
-        .arg("status")
-        .arg("--porcelain")
-        .output()
-        .expect("Failed to execute git status");
+  let output = Command::new("git")
+    .arg("status")
+    .arg("--porcelain")
+    .output()
+    .expect("Failed to execute git status");
 
-    String::from_utf8_lossy(&output.stdout)
-        .lines()
-        .map(String::from)
-        .collect()
+  String::from_utf8_lossy(&output.stdout).lines().map(String::from).collect()
 }
 
 fn run_git_add() -> bool {
-    let output = Command::new("git")
-        .arg("add")
-        .arg(".")
-        .output()
-        .expect("Failed to execute git add");
+  let output = Command::new("git").arg("add").arg(".").output().expect("Failed to execute git add");
 
-    output.status.success()
+  output.status.success()
 }
 
-fn run_git_commit() -> bool {
-    let output = Command::new("git")
-        .arg("commit")
-        .arg("--no-edit")
-        .output()
-        .expect("Failed to execute git commit");
+fn run_git_commit() -> Result<(), String> {
+  let output = Command::new("git").arg("commit").arg("--no-edit").output();
 
-    output.status.success()
+  match output {
+    Ok(output) => {
+      if output.status.success() {
+        Ok(())
+      } else {
+        let err_msg = String::from_utf8_lossy(&output.stderr).trim().to_string();
+        if err_msg.is_empty() {
+          Err(String::from("Git commit failed for an unknown reason."))
+        } else {
+          Err(err_msg)
+        }
+      }
+    },
+    Err(err) => Err(err.to_string()),
+  }
 }
 
-fn get_latest_commit_message() -> String {
-    let output = Command::new("git")
-        .arg("log")
-        .arg("-1")
-        .arg("--pretty=%B")
-        .output()
-        .expect("Failed to execute git log");
-
-    String::from_utf8_lossy(&output.stdout).trim().to_string()
+fn get_latest_commit_message() -> Result<String, git2::Error> {
+  let repo = Repository::open(".")?;
+  let head = repo.head()?;
+  let commit = head.peel_to_commit()?;
+  Ok(commit.message().unwrap_or("").to_string())
 }
 
 fn ensure_aicommit_hooks_installed() -> bool {
-    Path::new(".git/hooks/prepare-commit-msg").exists()
+  Path::new(".git/hooks/prepare-commit-msg").exists()
+}
+
+fn should_add_all() -> bool {
+  env::args().any(|arg| arg == "--all")
 }
