6b6626a4c27a6cd746aab7bb5e842482ef63dbb7
Linus Oleander <linus.oleander@clark.de>
2023-11-25 02:00:47 +0100

    Refactor diff function call to remove unnecessary error context

--- src/git.rs
+++ src/git.rs
@@ -120,7 +120,7 @@ impl Repo {
       index.write().context("Failed to write index")?;
     }
 
-    let (diff, _) = self.diff(1000).context("Failed to get diff")?;
+    let (diff, _) = self.diff(1000)?;
     let oid = index.write_tree().context("Failed to write tree")?;
     let tree = repo.find_tree(oid).context("Failed to find tree")?;
     let signature = repo.signature().context("Failed to get signature")?;
