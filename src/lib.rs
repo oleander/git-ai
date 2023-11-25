386c219bb153e965f2bbe69dec00b443b70dfc2c
Linus Oleander <linus.oleander@clark.de>
2023-11-25 01:51:25 +0100

    Remove unused imports in lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -3,10 +3,3 @@
 pub mod git;
 pub mod chat;
 
-use git::Repo;
-use std::os::unix::process::CommandExt;
-use std::process::{exit, Command};
-use dotenv::dotenv;
-use anyhow::Result;
-use clap::Parser;
-use log::error;
