dc0f18a8f66d293b646d499d1ad919d492c8f043
Linus Oleander <linus.oleander@clark.de>
2023-11-25 05:24:49 +0100

    Refactor max diff chars to use dotenv constant

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,8 @@ use dotenv::dotenv;
 use anyhow::Result;
 use colored::*;
 use clap::Parser;
+use lazy_static::lazy_static;
+use dotenv_codegen::dotenv;
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about)]
@@ -17,6 +19,11 @@ struct Cli {
   #[clap(long, default_value = "false", help = "git add .")]
   all: bool
 }
+
+lazy_static! {
+  static ref MAX_CHARS: usize = dotenv!("MAX_CHARS").parse::<usize>().unwrap();
+}
+
 
 #[tokio::main(flavor = "multi_thread")]
 async fn main() -> Result<()> {
@@ -30,7 +37,7 @@ async fn main() -> Result<()> {
   }
 
   let repo = git::Repo::new()?;
-  let (diff, files) = repo.diff(1000)?;
+  let (diff, files) = repo.diff(*MAX_CHARS)?;
   let message = chat::suggested_commit_message(diff).await?;
   let oid = repo.commit(&message, cli.all)?;
 
