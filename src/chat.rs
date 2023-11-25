3ef25f16b86ed6600607df7aa7b323647dbeb550
Linus Oleander <linus.oleander@clark.de>
2023-11-25 12:16:50 +0100

    Cleanup and refactor code for style consistency

--- src/chat.rs
+++ src/chat.rs
@@ -24,70 +24,69 @@ lazy_static! {
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 struct ChatMessage {
-    role: String,
-    content: String,
+  role:    String,
+  content: String
 }
 
 impl ChatMessage {
-    fn new(role: impl Into<String>, content: impl Into<String>) -> Self {
-        Self {
-            role: role.into(),
-            content: content.into(),
-        }
+  fn new(role: impl Into<String>, content: impl Into<String>) -> Self {
+    Self {
+      role: role.into(), content: content.into()
     }
+  }
 }
 
 #[derive(Error, Debug)]
 pub enum ChatError {
-    #[error("Failed to build HTTP client")]
-    HttpClientBuildError,
-    #[error("HTTP error: {0}")]
-    HttpRequestError(#[from] reqwest::Error),
-    #[error("IO error: {0}")]
-    IOError(#[from] io::Error),
-    #[error("Failed to parse JSON: {0}")]
-    JsonParseError(#[from] serde_json::Error),
-    #[error("Failed to extract message from response")]
-    ResponseExtractionError,
-    #[error("Anyhow error: {0}")]
-    Anyhow(#[from] anyhow::Error),
+  #[error("Failed to build HTTP client")]
+  HttpClientBuildError,
+  #[error("HTTP error: {0}")]
+  HttpRequestError(#[from] reqwest::Error),
+  #[error("IO error: {0}")]
+  IOError(#[from] io::Error),
+  #[error("Failed to parse JSON: {0}")]
+  JsonParseError(#[from] serde_json::Error),
+  #[error("Failed to extract message from response")]
+  ResponseExtractionError,
+  #[error("Anyhow error: {0}")]
+  Anyhow(#[from] anyhow::Error)
 }
 
 // Generates a commit message from the OpenAI API
 pub async fn generate_commit_message(diff: String) -> Result<String, ChatError> {
-    let prompt = format!(
+  let prompt = format!(
     "Generate a concise git commit message written in present tense for the following code diff with the given specifications below:\nMessage language: {:?}\nCommit message must be a maximum of {:?} characters.\nExclude anything unnecessary such as translation. Your entire response will be passed directly into git commit.",
     LANGUAGE, MAX_LENGTH
   );
 
-    let payload = json!({
-       "model": MODEL,
-       "messages": vec![
-         ChatMessage::new("system", prompt),
-         ChatMessage::new("user", diff)
-       ]
-    });
+  let payload = json!({
+     "model": MODEL,
+     "messages": vec![
+       ChatMessage::new("system", prompt),
+       ChatMessage::new("user", diff)
+     ]
+  });
 
-    let response = Client::builder()
-        .build()?
-        .post(API_URL)
-        .bearer_auth(API_KEY.as_str())
-        .json(&payload)
-        .timeout(Duration::from_secs(*TIMEOUT))
-        .send()
-        .await
-        .context("Failed to send request")?
-        .text()
-        .await
-        .context("Failed to get response body")
-        .and_then(|body| from_str::<Value>(&body).context("Failed to parse JSON"))?;
+  let response = Client::builder()
+    .build()?
+    .post(API_URL)
+    .bearer_auth(API_KEY.as_str())
+    .json(&payload)
+    .timeout(Duration::from_secs(*TIMEOUT))
+    .send()
+    .await
+    .context("Failed to send request")?
+    .text()
+    .await
+    .context("Failed to get response body")
+    .and_then(|body| from_str::<Value>(&body).context("Failed to parse JSON"))?;
 
-    let message = response["choices"]
-        .as_array()
-        .and_then(|choices| choices.get(0))
-        .and_then(|choice| choice["message"]["content"].as_str())
-        .map(|s| s.to_string())
-        .ok_or(ChatError::ResponseExtractionError)?;
+  let message = response["choices"]
+    .as_array()
+    .and_then(|choices| choices.get(0))
+    .and_then(|choice| choice["message"]["content"].as_str())
+    .map(|s| s.to_string())
+    .ok_or(ChatError::ResponseExtractionError)?;
 
-    Ok(message)
+  Ok(message)
 }
--- src/main.rs
+++ src/main.rs
@@ -13,57 +13,51 @@ use git::Repo;
 use lazy_static::lazy_static;
 use log::{debug, LevelFilter};
 
-#[macro_use]
 extern crate dotenv_codegen;
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about)]
 struct Cli {
-    #[clap(long, default_value = "false", help = "git add .")]
-    all: bool,
+  #[clap(long, default_value = "false", help = "git add .")]
+  all: bool,
 
-    #[clap(short, long, help = "Enables verbose logging", default_value = "false")]
-    verbose: bool,
+  #[clap(short, long, help = "Enables verbose logging", default_value = "false")]
+  verbose: bool
 }
 
 lazy_static! {
-    static ref MAX_CHARS: usize = dotenv!("MAX_CHARS").parse::<usize>().unwrap();
+  static ref MAX_CHARS: usize = dotenv!("MAX_CHARS").parse::<usize>().unwrap();
 }
 
 #[tokio::main(flavor = "multi_thread")]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    dotenv().ok();
+  dotenv().ok();
 
-    let cli = Cli::parse();
+  let cli = Cli::parse();
 
-    if cli.verbose {
-        env_logger::builder()
-            .filter_level(LevelFilter::Debug)
-            .format_target(false)
-            .format_timestamp(None)
-            .init();
-        debug!("Verbose logging enabled");
-    }
+  if cli.verbose {
+    env_logger::builder()
+      .filter_level(LevelFilter::Debug)
+      .format_target(false)
+      .format_timestamp(None)
+      .init();
+    debug!("Verbose logging enabled");
+  }
 
-    let repo = Repo::new()?;
+  let repo = Repo::new()?;
 
-    if cli.all {
-        repo.add_all()?;
-    }
+  if cli.all {
+    repo.add_all()?;
+  }
 
-    let (diff, files) = repo.diff(*MAX_CHARS)?;
-    let message = generate_commit_message(diff).await?;
-    let oid = repo.commit(&message)?;
+  let (diff, files) = repo.diff(*MAX_CHARS)?;
+  let message = generate_commit_message(diff).await?;
+  let oid = repo.commit(&message)?;
 
-    println!(
-        "{} [{:.7}] {}: ",
-        "🤖",
-        oid.to_string().yellow(),
-        message.green().italic()
-    );
-    for file in files {
-        println!("   {}", file.white());
-    }
+  println!("{} [{:.7}] {}: ", "🤖", oid.to_string().yellow(), message.green().italic());
+  for file in files {
+    println!("   {}", file.white());
+  }
 
-    Ok(())
+  Ok(())
 }
