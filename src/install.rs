2858d9aaa9ace3e9c6bea87e7128cad02781f006
Linus Oleander <linus.oleander@clark.de>
2023-12-04 04:44:39 +0100

    Refactor install logic and improve error handling

--- src/install.rs
+++ src/install.rs
@@ -1,11 +1,13 @@
 use git2::RepositoryOpenFlags as Flags;
-use anyhow::{Result, Context};
+use std::os::unix::fs::PermissionsExt;
 use std::path::{Path, PathBuf};
-use std::os::unix::fs::PermissionsExt; // This trait provides the set_mode method
+use anyhow::{Result, Context};
 use git2::Repository;
+use log::info;
 use std::env;
 use std::fs;
 
+// Git hook: prepare-commit-msg
 pub fn run() -> Result<()> {
   let current_dir = env::current_dir().with_context(|| "Failed to get current directory".to_string())?;
   let repo = Repository::open_ext(&current_dir, Flags::empty(), Vec::<&Path>::new())
@@ -20,17 +22,14 @@ pub fn run() -> Result<()> {
     fs::remove_file(&hook_file).with_context(|| format!("Failed to remove file: {:?}", hook_file))?;
   }
 
-  //   unix_fs::symlink(&binary_path, &hook_file).with_context(|| format!("Failed to create symlink: {:?}", hook_file))?;
-  // write the script to the file
   fs::write(&hook_file, script).with_context(|| format!("Failed to write file: {:?}", hook_file))?;
-
-  let metadata = fs::metadata(&hook_file)?;
+  let metadata = fs::metadata(&hook_file).context("Failed to get metadata")?;
   let mut permissions = metadata.permissions();
-  permissions.set_mode(0o755); // Read/write for owner and read for others.
-  fs::set_permissions(&hook_file, permissions)?;
+  permissions.set_mode(0o755);
+  fs::set_permissions(&hook_file, permissions).context("Failed to set permissions")?;
 
   let relative_path = hook_file.strip_prefix(&current_dir).context("Failed to strip prefix")?;
-  println!("Hook symlinked successfully to {:?}", relative_path);
+  info!("Hook symlinked successfully to {:?}", relative_path);
 
   Ok(())
 }
