09c03e8c329d5775c6639007a35394e51a1d65c5
Linus Oleander <linus.oleander@clark.de>
2023-12-09 23:09:32 +0100

    Improve hook.rs by removing unused imports and adjusting code structure

--- src/bin/hook.rs
+++ src/bin/hook.rs
@@ -2,19 +2,10 @@
 
 #![feature(assert_matches)]
 
-use std::io::{Read, Write};
-use std::time::Duration;
-use std::path::PathBuf;
-use std::fs::File;
 
 #[cfg(not(mock))]
 use ai::hook::Args;
-use git2::{DiffFormat, DiffOptions, Oid, Repository, Tree};
-use indicatif::{ProgressBar, ProgressStyle};
-use anyhow::{bail, Context, Result};
-use ai::chat::generate_commit;
-use lazy_static::lazy_static;
-use dotenv_codegen::dotenv;
+use anyhow::Result;
 use clap::Parser;
 
 #[tokio::main]
