613d1ffc4c4e12db6355ae1f3c009355308e0830
Linus Oleander <linus.oleander@clark.de>
2023-12-10 01:07:12 +0100

    Remove unused imports in hook module

--- src/hook/mod.rs
+++ src/hook/mod.rs
@@ -1,10 +1,5 @@
 // Hook: prepare-commit-msg
 pub mod traits;
 
-use std::path::PathBuf;
 
-use traits::*;
 
-use crate::chat::{generate_commit, ChatError};
-use crate::hook::traits::{FilePath, PatchRepository};
-use crate::config;
