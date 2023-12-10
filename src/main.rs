4b6d1a27ac5cc213de95e34908c2336a48e907fc
Your Name <you@example.com>
2023-12-10 05:35:50 +0100

    Update AppConfig struct and improve API key handling

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use config::Config;
+use std::env;
+
+use config::{Config, File, FileFormat};
 
 // APP_OPENAI_API_KEY="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"
 // APP_MODEL="gpt-4-1106-preview"
@@ -13,14 +15,21 @@ struct AppConfig {
   model:           Option<String>,
   max_diff_tokens: Option<u32>,
   max_length:      Option<u32>,
-  language:        Option<String>,
+  language:        String,
   timeout:         Option<u32>
 }
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
   dotenv::dotenv().ok();
+  let home_dir = env::var("HOME").expect("Failed to find HOME directory");
+  let bin_name = env::var("CARGO_PKG_NAME").unwrap_or("git-ai".to_owned());
+  let config_dir = format!("{home_dir}/.config/{bin_name}");
+  let config_file = format!("{config_dir}/config.ini");
+
   let config = Config::builder()
     .add_source(config::Environment::with_prefix("APP").try_parsing(true))
+    .add_source(config::File::new(config_file.as_str(), FileFormat::Ini))
+    .set_default("language", "en")?
     .build()
     .unwrap();
 
@@ -29,4 +38,5 @@ fn main() {
 
   println!("{:?}", app);
   // assert_eq!(
+    Ok(())
 }
