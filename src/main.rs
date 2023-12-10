967b43e27890216638303452b73a39b73637276f
Your Name <you@example.com>
2023-12-10 05:41:03 +0100

    Update config handling and add `home` crate dependency

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,35 @@
 use std::env;
 
-use config::{Config, File, FileFormat};
-
-// APP_OPENAI_API_KEY="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"
-// APP_MODEL="gpt-4-1106-preview"
-// APP_MAX_DIFF_TOKENS=5000
-// APP_MAX_LENGTH=72
-// APP_LANGUAGE=en
-// APP_TIMEOUT=30
+use config::{Config, FileFormat};
 
 #[derive(Debug, Default, serde_derive::Deserialize, PartialEq, Eq)]
 struct AppConfig {
-  openai_api_key:  Option<String>,
-  model:           Option<String>,
-  max_diff_tokens: Option<u32>,
-  max_length:      Option<u32>,
+  openai_api_key:  String,
+  model:           String,
+  max_diff_tokens: u32,
+  max_length:      u32,
   language:        String,
-  timeout:         Option<u32>
+  timeout:         u32
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
   dotenv::dotenv().ok();
-  let home_dir = env::var("HOME").expect("Failed to find HOME directory");
-  let bin_name = env::var("CARGO_PKG_NAME").unwrap_or("git-ai".to_owned());
-  let config_dir = format!("{home_dir}/.config/{bin_name}");
-  let config_file = format!("{config_dir}/config.ini");
+  let config_path = home::home_dir().unwrap().join(".config/git-ai/config.ini");
 
   let config = Config::builder()
     .add_source(config::Environment::with_prefix("APP").try_parsing(true))
-    .add_source(config::File::new(config_file.as_str(), FileFormat::Ini))
+    .add_source(config::File::new(config_path.to_str().unwrap(), FileFormat::Ini))
     .set_default("language", "en")?
+    .set_default("timeout", 30)?
+    .set_default("max_length", 72)?
+    .set_default("max_diff_tokens", 5000)?
+    .set_default("model", "gpt-4-1106-preview")?
     .build()
     .unwrap();
 
-  println!("{:?}", config);
   let app: AppConfig = config.try_deserialize().unwrap();
 
   println!("{:?}", app);
   // assert_eq!(
-    Ok(())
+  Ok(())
 }
