5bbb4b388e2c14cca869df91ad20c17fb84ed34f
Your Name <you@example.com>
2023-12-10 05:54:36 +0100

    Improve error handling and remove unused code in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 use std::env;
+use anyhow::Result;
+use dotenv::dotenv;
+use anyhow::Context;
 
 use config::{Config, FileFormat};
 
@@ -12,23 +15,27 @@ struct App {
   timeout:         usize
 }
 
+impl App {
+  pub fn new() -> Result<Self> {
+    let config_path = home::home_dir().unwrap().join(".config/git-ai/config.ini");
+
+    let config = Config::builder()
+      .add_source(config::Environment::with_prefix("APP").try_parsing(true))
+      .add_source(config::File::new(config_path.to_str().unwrap(), FileFormat::Ini))
+      .set_default("language", "en")?
+      .set_default("timeout", 30)?
+      .set_default("max_length", 72)?
+      .set_default("max_diff_tokens", 5000)?
+      .set_default("model", "gpt-4-1106-preview")?
+      .build()?;
+
+    config.try_deserialize().context("Failed to deserialize config")
+  }
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
   dotenv::dotenv().ok();
-  let config_path = home::home_dir().unwrap().join(".config/git-ai/config.ini");
-
-  let config = Config::builder()
-    .add_source(config::Environment::with_prefix("APP").try_parsing(true))
-    .add_source(config::File::new(config_path.to_str().unwrap(), FileFormat::Ini))
-    .set_default("language", "en")?
-    .set_default("timeout", 30)?
-    .set_default("max_length", 72)?
-    .set_default("max_diff_tokens", 5000)?
-    .set_default("model", "gpt-4-1106-preview")?
-    .build()?;
-
-  let app: App = config.try_deserialize()?;
-
-  println!("{:?}", app);
-  // assert_eq!(
+  let app = App::new().context("Failed to load config")?;
+  print!("{:?}", app);
   Ok(())
 }
