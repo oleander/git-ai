121629b62b44f98f3e57495c88026dd1cf3e6b08
Your Name <you@example.com>
2023-12-10 12:22:53 +0100

    Improve error handling in install process

--- src/install.rs
+++ src/install.rs
@@ -5,39 +5,48 @@ use std::{env, fs};
 use git2::{Repository, RepositoryOpenFlags as Flags};
 use anyhow::{bail, Context, Result};
 use log::debug;
+use thiserror::Error;
+#[derive(Error, Debug)]
+pub enum InstallError {
+  #[error("Failed to get current executable")]
+  CurrentExecutable,
+  #[error("Hook file at .git/hooks/prepare-commit-msg already exists, please remove it first using 'git ai hook uninstall'")]
+  HookFileExists,
+  #[error("Failed to get current directory")]
+  CurrentDirectory(#[from] std::io::Error),
+  #[error("Failed to open repository: {0}")]
+  OpenRepository(#[from] git2::Error),
+  #[error(transparent)]
+  Anyhow(#[from] anyhow::Error)
+}
 
 // Git hook: prepare-commit-msg
 // Crates an executable git hook (prepare-commit-msg) in the .git/hooks directory
-pub fn run() -> Result<()> {
-  let curr_bin = env::current_exe().context("Failed to get current executable")?;
+pub fn run() -> Result<(), InstallError> {
+  let curr_bin = env::current_exe()?;
   let exec_path = curr_bin.parent().context("Failed to get parent directory")?;
   let hook_bin = exec_path.join("git-ai-hook");
 
   if !hook_bin.exists() {
-    return Err(anyhow::anyhow!("Executable not found: {:?}", hook_bin));
+    return Err(InstallError::CurrentExecutable);
   }
-  // absolute path to this executable
-  // let script = include_bytes!("../target/debug/git-ai-hook");
 
-  let current_dir = env::current_dir().with_context(|| "Failed to get current directory".to_string())?;
-  debug!("Current directory: {:?}", current_dir);
-
-  let repo = Repository::open_ext(&current_dir, Flags::empty(), Vec::<&Path>::new())
-    .with_context(|| "Failed to open repository".to_string())?;
-  let git_path = repo.path().parent().context("Failed to get repository path")?.join(".git");
+  let current_dir = env::current_dir()?;
+  let repo = Repository::open_ext(current_dir, Flags::empty(), Vec::<&Path>::new())?;
+  let git_path = repo.path().parent().context("Failed to get repository path")?;
 
   let hook_dir = git_path.join("hooks");
   if !hook_dir.exists() {
-    fs::create_dir_all(&hook_dir).context("Failed to create .git/hooks")?;
+    fs::create_dir_all(&hook_dir)?;
   }
 
   let hook_file = hook_dir.join("prepare-commit-msg");
   if hook_file.exists() {
-    bail!("Hook file at .git/hooks/prepare-commit-msg already exists, please remove it first using 'git ai hook uninstall'");
+    return Err(InstallError::HookFileExists);
   }
 
   // Symlink the hook_bin to the hook_file
-  unix_fs::symlink(&hook_bin, &hook_file).with_context(|| format!("Failed to symlink {:?} to {:?}", hook_bin, hook_file))?;
+  unix_fs::symlink(&hook_bin, &hook_file)?;
 
   println!("Hook symlinked successfully to .git/hooks/prepare-commit-msg");
 
