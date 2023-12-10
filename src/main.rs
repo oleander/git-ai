13ebceb49e6f5670840fb4076b9c0924d82119e1
Your Name <you@example.com>
2023-12-10 07:35:33 +0100

    Update cargo config and improve CLI arg parsing and validation

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,9 @@ mod install;
 mod uninstall;
 mod config;
 
-use anyhow::{Context, Result};
+use std::str::FromStr;
+
+use anyhow::{bail, Context, Result};
 use dotenv::dotenv;
 use clap::{Arg, Command};
 
@@ -26,12 +28,41 @@ fn cli() -> Command {
             .subcommand(
               Command::new("timeout")
                 .about("Sets the timeout for the OpenAI API")
-                .arg(Arg::new("VALUE").required(true).index(1))
+                .arg(Arg::new("VALUE").required(true).index(1).value_parser(clap::value_parser!(usize)))
             )
             .subcommand(
-              Command::new("model")
-                .about("Sets the model to use")
-                .arg(Arg::new("VALUE").required(true).index(1))
+              Command::new("model").about("Sets the model to use").arg(
+                Arg::new("model")
+                  .required(true)
+                  .index(1)
+                  .value_parser(clap::builder::NonEmptyStringValueParser::new())
+              )
+            )
+            .subcommand(
+              Command::new("language").about("Sets the language to use").arg(
+                Arg::new("language")
+                  .required(true)
+                  .index(1)
+                  .value_parser(clap::builder::NonEmptyStringValueParser::new())
+              )
+            )
+            .subcommand(
+              Command::new("max-diff-tokens")
+                .about("Sets the maximum number of tokens to use for the diff")
+                .arg(Arg::new("VALUE").required(true).index(1).value_parser(clap::value_parser!(usize)))
+            )
+            .subcommand(
+              Command::new("max-length")
+                .about("Sets the maximum length of the commit message")
+                .arg(Arg::new("VALUE").required(true).index(1).value_parser(clap::value_parser!(usize)))
+            )
+            .subcommand(
+              Command::new("openai-api-key").about("Sets the OpenAI API key").arg(
+                Arg::new("openai-api-key")
+                  .required(true)
+                  .index(1)
+                  .value_parser(clap::builder::NonEmptyStringValueParser::new())
+              )
             )
         )
         .subcommand(
@@ -41,6 +72,7 @@ fn cli() -> Command {
         )
     )
 }
+
 #[tokio::main]
 async fn main() -> Result<()> {
   env_logger::init();
@@ -66,14 +98,22 @@ async fn main() -> Result<()> {
           let mut app = config::App::new()?;
           match args.subcommand() {
             Some(("timeout", args)) => {
-              app.timeout = args
-                .get_one::<String>("VALUE")
-                .context("Failed to get timeout value")?
-                .parse::<usize>()
-                .context("Failed to parse timeout value")?;
+              app.timeout = *args.get_one("<VALUE>").context("Failed to parse timeout")?;
             },
             Some(("model", args)) => {
-              app.model = args.get_one::<String>("VALUE").context("Failed to get model value")?.to_owned();
+              app.model = args.get_one::<String>("model").context("Failed to parse model")?.clone();
+            },
+            Some(("language", args)) => {
+              app.language = args.get_one::<String>("language").context("Failed to parse language")?.clone();
+            },
+            Some(("max-diff-tokens", args)) => {
+              app.max_diff_tokens = *args.get_one("<VALUE>").context("Failed to parse max-diff-tokens")?;
+            },
+            Some(("max-length", args)) => {
+              app.max_length = *args.get_one("<VALUE>").context("Failed to parse max-length")?;
+            },
+            Some(("openai-api-key", args)) => {
+              app.openai_api_key = args.get_one::<String>("openai-api-key").context("Failed to parse openai-api-key")?.clone();
             },
             _ => unreachable!()
           }
