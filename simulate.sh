7f3d7e4eb7f1545cb8a2f6ea89c4a9370f1402e4
Your Name <you@example.com>
2023-12-10 05:08:53 +0100

    Remove commented code and update simulate.sh script

--- simulate.sh
+++ simulate.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-BIN=$(pwd)/target/release/git-ai
-
 set -e
 
 git config --global user.email "you@example.com"
@@ -15,12 +13,13 @@ git add README.md
 git commit -m "Initial commit"
 echo "Hello World!" >> README.md
 git add README.md
-echo "git ai install"
-$BIN install
+echo "git ai hook install"
+git ai hook install
 echo "git commit"
 git commit --no-edit
 echo "git show"
 git --no-pager show HEAD 
 echo "git status"
 git status -s
+echo "git ai hook uninstall"
 rm -rf /tmp/git-ai
