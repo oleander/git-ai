836f07de8c1099cbefb4b70135335ba26dfb8143
Your Name <you@example.com>
2023-12-10 06:05:29 +0100

    Update dependencies, refactor config handling in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,31 @@
-use std::env;
-use anyhow::Result;
-use dotenv::dotenv;
-use anyhow::Context;
+use std::{io::Write, path::PathBuf};
+use std::fs::File;
 
+use serde::{Deserialize, Serialize};
 use config::{Config, FileFormat};
+use anyhow::{Context, Result};
+use lazy_static::lazy_static;
 
-#[derive(Debug, Default, serde_derive::Deserialize, PartialEq, Eq)]
-struct App {
-  openai_api_key:  String,
-  model:           String,
-  max_diff_tokens: usize,
-  max_length:      usize,
-  language:        String,
-  timeout:         usize
+#[derive(Debug, Default, Deserialize, PartialEq, Eq, Serialize)]
+pub struct App {
+  pub openai_api_key:  String,
+  pub model:           String,
+  pub language:        String,
+  pub max_diff_tokens: usize,
+  pub max_length:      usize,
+  pub timeout:         usize
+}
+
+lazy_static! {
+  pub static ref CONFIG_PATH: PathBuf = home::home_dir().unwrap().join(".config/git-ai/config.ini");
+  pub static ref APP: App = App::new().expect("Failed to load config");
 }
 
 impl App {
   pub fn new() -> Result<Self> {
-    let config_path = home::home_dir().unwrap().join(".config/git-ai/config.ini");
-
     let config = Config::builder()
       .add_source(config::Environment::with_prefix("APP").try_parsing(true))
-      .add_source(config::File::new(config_path.to_str().unwrap(), FileFormat::Ini))
+      .add_source(config::File::new(CONFIG_PATH.to_str().unwrap(), FileFormat::Ini))
       .set_default("language", "en")?
       .set_default("timeout", 30)?
       .set_default("max_length", 72)?
@@ -31,11 +35,21 @@ impl App {
 
     config.try_deserialize().context("Failed to deserialize config")
   }
+
+  pub fn save(&self) -> Result<()> {
+    let contents = serde_ini::to_string(&self).context("Failed to serialize config")?;
+    let mut file = File::create(CONFIG_PATH.to_str().unwrap()).context("Failed to create config file")?;
+    file.write_all(contents.as_bytes()).context("Failed to write config file")
+  }
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
   dotenv::dotenv().ok();
-  let app = App::new().context("Failed to load config")?;
+  let mut app = App::new().context("Failed to load config")?;
+  println!("{:?}", app.timeout);
+  app.timeout = 300;
+  app.save().context("Failed to save config")?;
+
   print!("{:?}", app);
   Ok(())
 }
