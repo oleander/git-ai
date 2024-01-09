8d4f511fa30d9537e6c48082c3502c577a892e9c
Linus Oleander <linus@oleander.io>
2024-01-10 00:55:17 +0100

    Update style.rs to handle prefix strip failure more gracefully

--- src/style.rs
+++ src/style.rs
@@ -7,7 +7,7 @@ pub trait Styled {
 impl Styled for PathBuf {
   fn relative_path(&self) -> PathBuf {
     let current_dir = env::current_dir().unwrap();
-    let relative_path = self.strip_prefix(&current_dir).unwrap();
+    let relative_path = self.strip_prefix(&current_dir).unwrap_or(current_dir.as_path());
     relative_path.to_path_buf()
   }
 }
