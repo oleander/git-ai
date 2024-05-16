a6da1cb30b6b51ae97dd70c0c4e2ce2b1b49b0f7
Linus Oleander <linus@oleander.io>
2024-05-16 21:47:48 +0200

    Refactor `run` function and error handling

--- src/install.rs
+++ src/install.rs
@@ -1,83 +1,29 @@
-use std::path::{Path, PathBuf};
-use std::{env, fs};
-
 use colored::Colorize;
-use ai::style::Styled;
+use anyhow::{bail, Result};
+use ai::filesystem::Filesystem;
 use console::Emoji;
-use git2::{Repository, RepositoryOpenFlags as Flags};
-use anyhow::{Context, Result};
-use thiserror::Error;
-
-#[derive(Error, Debug)]
-pub enum InstallError {
-  #[error("Failed to get current directory")]
-  CurrentDirectory(#[from] std::io::Error),
-  #[error(transparent)]
-  Anyhow(#[from] anyhow::Error),
-  #[error("Git error: {0}")]
-  Git(#[from] git2::Error),
-  #[error("Strip prefix error: {0}")]
-  StripPrefix(#[from] std::path::StripPrefixError),
-  #[error("Hook binary at {0} not found")]
-  HookBinNotFound(PathBuf),
-  #[error("Git hook already exists at {0}")]
-  GitHookExists(PathBuf),
-
-  #[error("Git repository not found at {0}")]
-  GitRepoNotFound(PathBuf)
-}
 
 const EMOJI: Emoji<'_, '_> = Emoji("🔗", "");
 
-fn can_override_hook() -> bool {
-  std::env::args()
-    .collect::<Vec<String>>()
-    .iter()
-    .any(|arg| arg == "-f")
-}
+pub fn run() -> Result<()> {
+  env_logger::init();
 
-// Git hook: prepare-commit-msg
-// Crates an executable git hook (prepare-commit-msg) in the .git/hooks directory
-pub fn run() -> Result<(), InstallError> {
-  let curr_bin = env::current_exe()?;
-  let exec_path = curr_bin
-    .parent()
-    .context("Failed to get parent directory")?;
-  let hook_bin = exec_path.join("git-ai-hook");
+  let filesystem = Filesystem::new()?;
 
-  // Check if the hook binary exists
-  if !hook_bin.exists() {
-    return Err(InstallError::HookBinNotFound(hook_bin));
+  if !filesystem.git_hooks_path().exists() {
+    filesystem.git_hooks_path().create_dir_all()?;
   }
 
-  let current_dir = env::current_dir()?;
-  let repo = Repository::open_ext(current_dir, Flags::empty(), Vec::<&Path>::new())?;
-  let repo_path = repo
-    .path()
-    .parent()
-    .context("Failed to get parent directory")?;
-  let git_path = match repo_path.file_name() {
-    Some(name) if name == ".git" => repo_path.to_path_buf(),
-    Some(_) => repo_path.join(".git"),
-    None => return Err(InstallError::GitRepoNotFound(repo_path.to_path_buf()))
-  };
+  let hook_file = filesystem.prepare_commit_msg_path()?;
+  let hook_bin = filesystem.git_ai_hook_bin_path()?;
 
-  let hook_dir = git_path.join("hooks");
-  if !hook_dir.exists() {
-    fs::create_dir_all(&hook_dir)?;
+  if hook_file.exists() {
+    bail!("Hook already exists at {}, please run 'git ai hook reinstall'", hook_file);
   }
 
-  let hook_file = hook_dir.join("prepare-commit-msg");
-  if hook_file.exists() && !can_override_hook() {
-    return Err(InstallError::GitHookExists(hook_file.relative_path()));
-  }
+  hook_file.symlink(hook_bin)?;
 
-  std::fs::copy(&hook_bin, &hook_file)?;
-
-  println!(
-    "{EMOJI} Hook symlinked successfully to {}",
-    hook_file.relative_path().display().to_string().italic()
-  );
+  println!("{EMOJI} Hook symlinked successfully to {}", hook_file.to_string().italic());
 
   Ok(())
 }
